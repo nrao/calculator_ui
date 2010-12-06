405b8e7ac1adcdfd4b41451b194815733a850f56
Mike McCarty <mmccarty@nrao.edu>
2010-12-06 16:00:11 -0500

    finished with UI changes for source information

--- src/edu/nrao/dss/client/SourceForm.java
+++ src/edu/nrao/dss/client/SourceForm.java
@@ -4,82 +4,159 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import com.extjs.gxt.ui.client.Style;
+import com.extjs.gxt.ui.client.Style.Orientation;
 import com.extjs.gxt.ui.client.event.Events;
 import com.extjs.gxt.ui.client.event.FieldEvent;
 import com.extjs.gxt.ui.client.event.Listener;
 import com.extjs.gxt.ui.client.widget.Slider;
 import com.extjs.gxt.ui.client.widget.form.CheckBox;
+import com.extjs.gxt.ui.client.widget.form.Field;
+import com.extjs.gxt.ui.client.widget.form.Radio;
+import com.extjs.gxt.ui.client.widget.form.RadioGroup;
+import com.extjs.gxt.ui.client.widget.form.SliderField;
+import com.extjs.gxt.ui.client.widget.form.Validator;
+import com.extjs.gxt.ui.client.widget.layout.FormData;
 import com.google.gwt.core.client.GWT;
 public class SourceForm extends BasicForm {
-	private GeneralText sourceDec, minElev, restFreq, sourceVelocity, redshift,
+	private GeneralText sourceDec, restFreq, sourceVelocity, redshift,
 			topoFreq, rightAscension, tBG;
-	private GeneralCombo frame, doppler, size;
-	private CheckBox galactic;
-
+	private GeneralCombo doppler, size;
+	private RadioGroup galactic, frame;
+	private Slider diameter;
+	
 	public SourceForm() {
 		super("Source Information");
 	}
 
 	public void initLayout() {
 		setCollapsible(true);
-		sourceDec = new GeneralText("sourceDec", "Source Declination");
-		minElev = new GeneralText("minElev", "Minimum Elevation");
-		frame = new GeneralCombo("frame", "Frames", getFrames());
-		restFreq = new GeneralText("restFreq", "Rest Frequency");
-		topoFreq = new GeneralText("topoFreq", "Topocentric Frequency");
-		doppler = new GeneralCombo("doppler", "Doppler Correction",
-				getDoppler());
-		sourceVelocity = new GeneralText("sourceVelocity", "Source Velocity");
+		sourceDec = new GeneralText("declination", "Source Declination");
+		sourceDec.setValue("38.43");
+		restFreq  = new GeneralText("rest_freq", "Rest Frequency (MHz)");
+		topoFreq  = new GeneralText("topocentric_freq", "Topocentric Frequency (MHz)");
+		doppler   = new GeneralCombo("doppler", "Doppler Correction", getDoppler());
+		doppler.setValue(new ComboModel("Optical"));
+		sourceVelocity = new GeneralText("source_velocity", "Source Velocity (km/s)");
+		sourceVelocity.setValue("0");
 		redshift = new GeneralText("redshift", "Redshift");
-		size = new GeneralCombo("size", "Source Size", new ArrayList<String>(
-				Arrays.asList("Point source", "Diameter < 2 FWHM",
-						"Diameter > 2 FWHM")));
-		galactic = new CheckBox();
-		galactic.setBoxLabel("Add contribution from Galactic Continuum");
-		galactic.setLabelSeparator("");
-		rightAscension = new GeneralText("rightAscention",
+		redshift.setValue("0");
+		size     = new GeneralCombo("source_size", "Source Size", new ArrayList<String>(
+				             Arrays.asList("Point source", "Diameter < 2 FWHM", "Diameter > 2 FWHM")));
+		
+		frame = new RadioGroup("frame");
+		frame.setFieldLabel("Frame");
+		frame.setName("frame");
+		frame.setId("frame");
+		frame.setOrientation(Orientation.VERTICAL);
+		
+		Radio choice = new Radio();
+		choice.setBoxLabel("Topocentric Frame");
+		choice.setValueAttribute("Topocentric Frame");
+		choice.setName("topocentric_frame");
+		choice.setLabelSeparator("");
+		frame.add(choice);
+		
+		choice = new Radio();
+		choice.setBoxLabel("Rest Frame");
+		choice.setValueAttribute("Rest Frame");
+		choice.setName("rest_frame");
+		choice.setLabelSeparator("");
+		choice.setValue(true);
+		frame.add(choice);
+		
+		galactic = new RadioGroup("galactic");
+		galactic.setFieldLabel("Source Contribution Corrections");
+		galactic.setName("galactic");
+		galactic.setId("galactic");
+		galactic.setOrientation(Orientation.VERTICAL);
+		
+		choice = new Radio();
+		choice.setBoxLabel("No Correction");
+		choice.setToolTip("No correction for the sources contribution.");
+		choice.setValueAttribute("no_correction");
+		choice.setName("no_correction");
+		choice.setLabelSeparator("");
+		choice.setValue(true);
+		galactic.add(choice);
+		
+		choice = new Radio();
+		choice.setBoxLabel("Estimated");
+		choice.setToolTip("Enter your own estimate for the contribution from Galactic Continuum.");
+		choice.setValueAttribute("estimated");
+		choice.setName("estimated");
+		choice.setLabelSeparator("");
+		galactic.add(choice);
+		
+		choice = new Radio();
+		choice.setBoxLabel("Modeled");
+		choice.setToolTip("Use a standard model for the contribution from Galactic Continuum.");
+		choice.setValueAttribute("model");
+		choice.setName("model");
+		choice.setLabelSeparator("");
+		galactic.add(choice);
+		
+		diameter = new Slider();
+		diameter.setMinValue(4);
+		diameter.setMaxValue(20);
+		diameter.setValue(4);
+		diameter.setIncrement(1);
+		diameter.setMessage("{0} 10^-1 arc minutes");
+		
+		final SliderField sf = new SliderField(diameter);
+		sf.setFieldLabel("Angular Diameter");
+		sf.setName("angular_diameter");
+		sf.setId("angular_diameter");
+		
+		rightAscension = new GeneralText("right_ascension",
 				"Enter right ascension");
-		tBG = new GeneralText("tBG",
+		tBG = new GeneralText("estimated_continuum",
 				"Enter contribution of source to continuum level");
+		tBG.setValue("0");
         
 		//sourceDec.setRegex("[1-9][0-9]{2}\\.[0-9]{2}","Invalid format");
-		sourceDec.setRegex("\\d\\d\\.\\d\\d","Invalid format");
+		sourceDec.setRegex("([0-9\\,\\.\\+\\-]+)","Invalid format: Must be a number.");
 		sourceDec.setMessageTarget("side");
-		//sourceDec.setRegexText("sdf");
-		//sourceDec.setAllowBlank(false);
-		///sourceDec.forceInvalid("ahhahl");
-		//FieldMessages me = new FieldMessages();
-		//me.setInvalidText("wrong!!");
-		//sourceDec.setMessages(me);
-		//FieldMessages me =sourceDec.new FieldMessages();
-		//me.setInvalidText("AHAH");
-		//sourceDec.setMessages(me);
-		// initial state of questions
-		//Validator jk = sourceDec.getValidator();
-		
-		restFreq.hide();
-		doppler.hide();
-		redshift.hide();
-		sourceVelocity.hide();
-		rightAscension.hide();
+		sourceDec.setValidator(new Validator () {
 
-		frame.addListener(Events.Select, new HandleFrame());
+			@Override
+			public String validate(Field<?> field, String value) {
+				if (value.isEmpty() || value.equals("-")) {
+					return null;
+				}
+				
+				float min_el = (float) 0.0;
+				float dec = (Float.valueOf(value).floatValue() + min_el);
+				if (dec <= -51.57) {
+					return "Declination does not achieve the desired minimum elevation for the chosen receiver and frequency.";
+				}
+				return null;
+			}
+			
+		});
+		
+		topoFreq.hide();
+		topoFreq.setAllowBlank(true);
+		redshift.hide();
+		redshift.setAllowBlank(true);
+		rightAscension.hide();
+		rightAscension.setAllowBlank(true);
+		tBG.hide();
+		tBG.setAllowBlank(true);
+
+		frame.addListener(Events.Change, new HandleFrame());
 		doppler.addListener(Events.Select, new HandleDoppler());
 		galactic.addListener(Events.Change, new HandleGalactic());
 
 		// attaching fields
 		add(sourceDec);
-		add(minElev);
 		add(frame);
 		add(restFreq);
 		add(topoFreq);
-		add(redshift);
 		add(doppler);
 		add(redshift);
 		add(sourceVelocity);
-		// add(new Label("Source Size:"));
-		// add(getSlider());
 		add(size);
+		add(sf);
 		add(galactic);
 		add(rightAscension);
 		add(tBG);
@@ -90,59 +167,67 @@ public class SourceForm extends BasicForm {
 
 	}
 
-	// TODO
-	// Modify this to fit into the design
-	// Sliders arn't Fields
-	// Work around: maybe create a hidden field that observes
-	// the value of the slider
-	public Slider getSlider() {
-		Slider slider = new Slider();
-		slider.setBounds(10, 10, 200, Style.DEFAULT);
-		slider.setMinValue(1);
-		slider.setMaxValue(100);
-		slider.setIncrement(1);
-		slider.setValue(100);
-		slider.setMessage("Scale by {0}%");
-		return slider;
-	}
-
 	public void notify(String name, String value) {
 		// handler for mode
 		if (name.equals("mode")) {
 			if (value.equals("Spectral Line")) {
 				frame.show();
-				restFreq.hide();
-				doppler.hide();
-				topoFreq.show();
+				topoFreq.hide();
+				topoFreq.setAllowBlank(true);
+				restFreq.show();
+				restFreq.setAllowBlank(false);
+				doppler.show();
 				redshift.hide();
-				sourceVelocity.hide();
+				redshift.setAllowBlank(true);
+				sourceVelocity.show();
+				sourceVelocity.setAllowBlank(false);
 			} else {
 				frame.hide();
 				restFreq.hide();
+				restFreq.setAllowBlank(true);
 				doppler.hide();
 				topoFreq.show();
+				topoFreq.setAllowBlank(false);
 				redshift.hide();
+				redshift.setAllowBlank(true);
 				sourceVelocity.hide();
+				sourceVelocity.setAllowBlank(true);
 			}
 
+		} else if (name.equals("receiver")) {
+			if (value.contains("(")) {
+				float freq_low = Float.valueOf(value.substring(value.indexOf("(") + 1, value.indexOf("-") - 1)).floatValue();
+				float freq_hi  = Float.valueOf(value.substring(value.indexOf("-") + 2, value.indexOf(")"))).floatValue();
+				float freq_mid = (freq_low + (freq_hi - freq_low) / 2) * 1000;
+				restFreq.setValue("" + freq_mid);
+				topoFreq.setValue("" + freq_mid);
+			}
 		}
 
 	}
 
 	class HandleFrame implements Listener<FieldEvent> {
 		public void handleEvent(FieldEvent fe) {
-			if (frame.getValue().getName("name") == "Rest Frame") {
+			if (frame.getValue().getValueAttribute().equals("Rest Frame")) {
 				topoFreq.hide();
+				topoFreq.setAllowBlank(true);
 				restFreq.show();
+				restFreq.setAllowBlank(false);
 				doppler.show();
 				sourceVelocity.show();
+				sourceVelocity.setAllowBlank(false);
 				redshift.hide();
+				redshift.setAllowBlank(true);
 			} else {
 				topoFreq.show();
+				topoFreq.setAllowBlank(false);
 				restFreq.hide();
+				restFreq.setAllowBlank(true);
 				doppler.hide();
 				sourceVelocity.hide();
+				sourceVelocity.setAllowBlank(true);
 				redshift.hide();
+				redshift.setAllowBlank(true);
 			}
 
 		}
@@ -152,31 +237,41 @@ public class SourceForm extends BasicForm {
 		public void handleEvent(FieldEvent fe) {
 			if (doppler.getValue().getName("name") == "RedShift") {
 				redshift.show();
+				redshift.setAllowBlank(false);
 				sourceVelocity.hide();
+				sourceVelocity.setAllowBlank(true);
 			} else {
 				redshift.hide();
+				redshift.setAllowBlank(true);
 				sourceVelocity.show();
+				sourceVelocity.setAllowBlank(false);
 			}
 		}
 	}
 
 	class HandleGalactic implements Listener<FieldEvent> {
 		public void handleEvent(FieldEvent fe) {
-			if (galactic.getValue()) {
+			if (galactic.getValue().getValueAttribute().equals("model")) {
 				rightAscension.show();
+				rightAscension.setAllowBlank(false);
+				tBG.hide();
+				tBG.setAllowBlank(true);
+			} else if (galactic.getValue().getValueAttribute().equals("estimated")) {
+				rightAscension.hide();
+				rightAscension.setAllowBlank(true);
+				tBG.show();
+				tBG.setAllowBlank(false);
 			} else {
 				rightAscension.hide();
+				rightAscension.setAllowBlank(true);
+				tBG.hide();
+				tBG.setAllowBlank(true);
 			}
 		}
 	}
-
-	public static ArrayList<String> getFrames() {
-		return new ArrayList<String>(Arrays.asList("Topocentric Frame",
-				"Rest Frame"));
-	}
-
+	
 	public static ArrayList<String> getDoppler() {
-		return new ArrayList<String>(Arrays.asList("Radio", "Doppler",
+		return new ArrayList<String>(Arrays.asList("Radio", "Optical",
 				"RedShift"));
 	}
 
